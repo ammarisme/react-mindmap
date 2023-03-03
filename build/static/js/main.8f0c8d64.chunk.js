cdf4ef1f28d08b9abab8721d65f9422ba3086ce4
Ammar <ammar@WSO2s-MacBook-Pro.local>
2023-03-04 00:57:34 +0530

    removed react mind map from the url

--- build/static/js/main.8f0c8d64.chunk.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonpreact-mindmap"]=window["webpackJsonpreact-mindmap"]||[]).push([[4],{353:function(n,p,a){n.exports=a(902)}},[[353,5,0,1,3,2]]]);
-//# sourceMappingURL=main.8f0c8d64.chunk.js.map
